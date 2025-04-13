dae7bebdfde87337c484cff335eb9bab765a2f34
Oleksandr Yanchar <contact.oleksandryanchar@gmail.com>
2025-04-13 15:05:35 +0300

    UPDATE

--- backend/src/api/v1/drf/urls.py
+++ backend/src/api/v1/drf/urls.py
@@ -2,4 +2,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("auth/", include("src.api.v1.drf.auth.urls")),
+    path("profiles/", include("src.api.v1.drf.profiles.urls")),
+
 ]
